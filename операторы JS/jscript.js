3b0948639f8b199c2e39fa8f75cd837ab349ab95
Natali <tsibinanatali@gmail.com>
2025-03-17 21:19:00 +0300

    Add files via upload

---
+++ операторы JS/jscript.js
@@ -0,0 +1,18 @@
+ //1 символ -используется для создания уникальных идентификаторами
+ //id -
+//  const sym1 = Symbol.for("id");   //неуникальное значение
+//  const sym2 = Symbol("id");       //уникальное значение
+//  console.log(sym1 === sym2);
+ //приметивные типы -это базовые типы данных( числа-Number,,e )
+ //объектные типы данных
+ let positiveInfiniti = 1/ 0;
+ console.log(positiveInfiniti);
+ let person={name:"john",age:25};
+ console.log(person);
+ let numbers = [1,2,3,4,5];
+ function greet(name){
+    return"привет, " + name + "!";
+ 
+ console.log(greet("ann"));
+}
+          
