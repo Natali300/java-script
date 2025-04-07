b7e97f3e60ad6ed3a392fa7cf4c106c4df4724cc
Natali <tsibinanatali@gmail.com>
2025-04-07 21:00:39 +0300

    Add files via upload

---
+++ математика/script.js
@@ -0,0 +1,21 @@
+//математические методы
+Math. PI
+console.log(Math.PI);
+console.log(Math.round(4.6));//округление до ближайшего целого
+number = 4.566767889;
+let mum1 = number.toFixed(2);//округляет кол заков после запятой
+console.log(num1);
+console.log(Math.floor(4.9));//округление вниз(4)
+console.log(Math.ceil(5.1));//округление вверх(6)
+console.log(Math.abs(-7));//модуль числа (7)
+console.log(Math.pow(2, 3));//возведение в степень.2 - какое число 3 - в какую степень
+console.log(Math.max(1,2,3));
+console.log(Math.min(1,2,3));
+console.log(Math.sqrt(4));// квадратный корень
+
+
+let random = Math.floor(Math.random()*10)+1;
+console.log(random);
+
+
+
